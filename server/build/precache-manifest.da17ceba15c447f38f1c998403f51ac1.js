426a8d074b0c9d1c22d305c209ddc0bf7550acb1
Julius Uusinarkaus <julius@uusinarkaus.com>
2024-10-26 15:32:10 +0300

    move client and server to monorepo

---
+++ server/build/precache-manifest.da17ceba15c447f38f1c998403f51ac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ff51f61336b3b68c52b7591ff37deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "648aed8840b6b6578c9d",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a52498ae2c5574196857",
+    "url": "/static/css/main.84df229e.chunk.css"
+  },
+  {
+    "revision": "648aed8840b6b6578c9d",
+    "url": "/static/js/2.c84da5c1.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.c84da5c1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a52498ae2c5574196857",
+    "url": "/static/js/main.6688c585.chunk.js"
+  },
+  {
+    "revision": "a926956ab92474d06e88",
+    "url": "/static/js/runtime-main.6a87530c.js"
+  }
+]);
